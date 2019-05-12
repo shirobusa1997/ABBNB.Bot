071ca4c87674b0ed5a5d852ca70860df75cc272f
MOJATTO STUDIO <daichiandroid1997@gmail.com>
2019-05-13 05:18:17 +0900

    必要リソースを追加しました。

---
+++ info.py
@@ -0,0 +1,2 @@
+# Current Bot Token
+BOT_TOKEN='NDYzMDU5MzY3NjkyODYxNDQx.XNhzwA.RBLemb_vubl_fbcFViH9Pt_qjN8'
---
+++ pyar.py
@@ -0,0 +1,37 @@
+import discord
+import asyncio
+
+import info
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+	print('READY')
+
+@client.event
+async def on_message(message):
+	# あべべのべ自身の発言は無視する(無限ループを回避するため)
+	if client == message.author:
+		return
+
+	if message.content.startswith("タンヤオ" or "!tanyao"):
+		await client.send_file(message.channel, 'Resources/tanyao.png')
+
+	if message.content.startswith("あべべ"):
+		# await message.channel.send("あべ...あべべ...")
+
+	if message.content.startswith('!pyar' or "ぴゃあ"):
+		# await message.channel.send("...!!")
+		await client.send_file(message.channel, 'Resources/tanyao.png')
+
+	if message.content.startswith('!ssr' or "SSR"):
+		# m = "..."
+		# await message.channel.send(m)
+		await client.send_file(message.channel, 'Resources/ssr_drawn.mp4')
+
+	if message.content.startswith('!4pp' or "ﾌｫｰﾊﾟｰｿﾝﾌﾟﾚｰｲ"):
+		# await message.channel.send("...")
+		await client.send_file(message.channel, 'Resources/4pp.JPG')
+
+client.run(info.BOT_TOKEN)
